68f33007c2b13018fadfe39c583c61f9b78d0c94
Утемов Александр <utemovaleks@gmail.com>
2026-03-12 05:25:00 +0500

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import BoardPresenter from './presenter/board-presenter.js';
+import PointsModel from './model/points-model.js';
 
 const tripEventsContainer = document.querySelector('.trip-events');
-const tripPresenter = new BoardPresenter({ container: tripEventsContainer });
+const pointsModel = new PointsModel();
+
+const tripPresenter = new BoardPresenter({
+  container: tripEventsContainer,
+  pointsModel
+});
 
 tripPresenter.init();
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,14 +3,12 @@ import SortView from '../view/sort-view.js';
 import EventListView from '../view/event-list-view.js';
 import PointView from '../view/point-view.js';
 import PointEditView from '../view/edit-point-view.js';
-import CreationFormView from '../view/create-form-view.js';
 import { render } from '../render.js';
-
-const POINT_COUNT = 3;
 
 export default class BoardPresenter {
   constructor(config) {
     this.container = config.container;
+    this.pointsModel = config.pointsModel;
     this.filterContainer = document.querySelector('.trip-controls__filters');
     this.filterComponent = new FilterView();
     this.sortComponent = new SortView();
@@ -18,16 +16,37 @@ export default class BoardPresenter {
   }
 
   init() {
+    this.boardPoints = [...this.pointsModel.getPoints()];
+    this.destinations = [...this.pointsModel.getDestinations()];
+    this.offers = [...this.pointsModel.getOffers()];
+
     render(this.filterComponent, this.filterContainer);
     render(this.sortComponent, this.container);
     render(this.eventListComponent, this.container);
 
     const eventsListElement = this.eventListComponent.getElement();
-    render(new PointEditView(), eventsListElement);
-    render(new CreationFormView(), eventsListElement);
 
-    for (let i = 0; i < POINT_COUNT; i++) {
-      render(new PointView(), eventsListElement);
+    const firstPoint = this.boardPoints[0];
+    const firstDestination = this.destinations.find((d) => d.id === firstPoint.destination);
+    const firstOffers = this.offers.find((o) => o.type === firstPoint.type).offers;
+
+    render(new PointEditView({
+      point: firstPoint,
+      destination: firstDestination,
+      offers: firstOffers
+    }), eventsListElement);
+
+    for (let i = 1; i < this.boardPoints.length; i++) {
+      const point = this.boardPoints[i];
+      const destination = this.destinations.find((d) => d.id === point.destination);
+      const typeOffers = this.offers.find((o) => o.type === point.type).offers;
+      const pointOffers = typeOffers.filter((offer) => point.offers.includes(offer.id));
+
+      render(new PointView({
+        point,
+        destination,
+        offers: pointOffers
+      }), eventsListElement);
     }
   }
 }
