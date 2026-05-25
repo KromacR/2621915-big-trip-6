7936391d4c9ba6db770e95142df9be5187fb51d0
Утемов Александр <utemovaleks@gmail.com>
2026-05-26 04:24:18 +0500

    Добавил autocomplete="off"

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -102,7 +102,7 @@ const createEditFormTemplate = (state, destination, allOffers) => {
             <label class="event__label  event__type-output" for="event-destination-1">
               ${type}
             </label>
-            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1">
+            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1" autocomplete="off">
             <datalist id="destination-list-1">
               <option value="Amsterdam"></option>
               <option value="Geneva"></option>
